9ed6931d82796bc3c657ff2c9a0ad1f5c70e0674
Eric Suarez <egs104@gmail.com>
2016-01-19 13:27:32 -0500

    created schema and set URI for mongodb

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,37 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config.js');
 
 /* Connect to your database */
+mongoose.connect('mongodb://egs104:brandy@ds047075.mongolab.com:47075/ericryandb');
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+// MongoClient.connect('mongodb://egs104:brandy@ds047075.mongolab.com:47075/ericryandb', function(err, db){
+//
+//   if (err) throw err;
+//
+// });
+
+
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
 
+var newListing = Listing({
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+
+
+})
+
+
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
